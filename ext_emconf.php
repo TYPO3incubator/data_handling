dc107d7d6a04a8e57b6abd097a25c48f9f577d23
Oliver Hader <oliver@typo3.org>
2016-06-20 14:15:08 +0200

    [TASK] Initial composer settings

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'TYPO3 DataHandling',
+    'description' => 'Data Handking for TYPO3 CMS',
+    'category' => 'misc',
+    'state' => 'alpha',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Oliver Hader',
+    'author_email' => 'oliver.hader@typo3.org',
+    'author_company' => '',
+    'version' => '8.0.0',
+    'constraints' => array(
+        'depends' => array(),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+);

563ecd3dd517da6ecb395d913f3a0a70b28c2c7a
Oliver Hader <oliver@typo3.org>
2016-08-06 21:38:19 +0200

    [TASK] Extend meta-model mapping test-case

--- Tests/Functional/Core/MetaModel/MapTest.php
+++ Tests/Functional/Core/MetaModel/MapTest.php
@@ -14,10 +14,22 @@ namespace TYPO3\CMS\DataHandling\Tests\Functional\Core\MetaModel;
  * The TYPO3 project - inspiring people to share!
  */
 
+use TYPO3\CMS\Core\Tests\FunctionalTestCase;
+use TYPO3\CMS\DataHandling\Core\MetaModel\ActiveRelation;
 use TYPO3\CMS\DataHandling\Core\MetaModel\Map;
+use TYPO3\CMS\DataHandling\Core\MetaModel\PassiveRelation;
+use TYPO3\CMS\DataHandling\Core\MetaModel\Relational;
+use TYPO3\CMS\Extbase\Reflection\ObjectAccess;
 
-class MapTest
+class MapTest extends FunctionalTestCase
 {
+    /**
+     * @var string[]
+     */
+    protected $testExtensionsToLoad = [
+        'typo3/sysext/core/Tests/Functional/Fixtures/Extensions/irre_tutorial'
+    ];
+
     /**
      * @var Map
      */
@@ -25,12 +37,34 @@ class MapTest
 
     protected function setup()
     {
-        $this->map = Map::instance();
+        parent::setUp();
+        $this->subject = Map::instance();
     }
 
     protected function tearDown()
     {
-        unset($this->map);
+        unset($this->subject);
+        parent::tearDown();
+    }
+
+    /**
+     * @test
+     */
+    public function invalidSchemaReturnsNull()
+    {
+        $this->assertNull(
+            $this->subject->getSchema(uniqid('anything'))
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function invalidPropertyReturnsNull()
+    {
+        $this->assertNull(
+            $this->subject->getSchema('tt_content')->getProperty(uniqid('anything'))
+        );
     }
 
     /**
@@ -38,6 +72,78 @@ class MapTest
      */
     public function fileReferenceRelationsAreDefined()
     {
-        var_dump('x');
+        $this->assertHasActiveRelation(
+            [
+                'property.schema.name' => 'tt_content',
+                'property.name' => 'image',
+                'to.name' => 'sys_file_reference',
+            ],
+            $this->subject->getSchema('tt_content')->getProperty('image')->getRelations()
+        );
+        $this->assertHasPassiveRelation(
+            [
+                'property.schema.name' => 'sys_file_reference',
+                'property.name' => 'uid_foreign',
+                'from.schema.name' => 'tt_content',
+                'from.name' => 'image',
+            ],
+            $this->subject->getSchema('sys_file_reference')->getProperty('uid_foreign')->getRelations()
+        );
+    }
+
+    /**
+     * @param array $expectations
+     * @param Relational[] $actualRelations
+     */
+    protected function assertHasActiveRelation(array $expectations, array $actualRelations)
+    {
+        $found = false;
+        foreach ($actualRelations as $actualRelation) {
+            if (!($actualRelation instanceof ActiveRelation)) {
+                continue;
+            }
+            if (!$this->matchesExpectations($expectations, $actualRelation)) {
+                continue;
+            }
+            $found = true;
+            break;
+        }
+        $this->assertTrue($found, 'No relation found that matches expectations.');
+    }
+
+    /**
+     * @param array $expectations
+     * @param Relational[] $actualRelations
+     */
+    protected function assertHasPassiveRelation(array $expectations, array $actualRelations)
+    {
+        $found = false;
+        foreach ($actualRelations as $actualRelation) {
+            if (!($actualRelation instanceof PassiveRelation)) {
+                continue;
+            }
+            if (!$this->matchesExpectations($expectations, $actualRelation)) {
+                continue;
+            }
+            $found = true;
+            break;
+        }
+        $this->assertTrue($found, 'No relation found that matches expectations.');
+    }
+
+    /**
+     * @param array $expectations
+     * @param object|array|\ArrayAccess $subject
+     * @return bool
+     */
+    protected function matchesExpectations(array $expectations, $subject): bool
+    {
+        $matches = 0;
+        foreach ($expectations as $expectationPath => $expectationValue) {
+            if (ObjectAccess::getPropertyPath($subject, $expectationPath) === $expectationValue) {
+                $matches++;
+            }
+        }
+        return ($matches === count($expectations));
     }
 }

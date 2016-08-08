82cdf08651af45e780de0e2046ab7a20e2c85541
Oliver Hader <oliver@typo3.org>
2016-08-08 21:17:04 +0200

    [FEATURE] Add SortingComparisonService

---
+++ Classes/Core/Service/SortingComparisonService.php
@@ -0,0 +1,128 @@
+<?php
+namespace TYPO3\CMS\DataHandling\Core\Service;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class SortingComparisonService implements SingletonInterface
+{
+    const ACTION_ADD = 'add';
+    const ACTION_REMOVE = 'remove';
+    const ACTION_ORDER = 'order';
+
+    /**
+     * @return SortingComparisonService
+     */
+    static public function instance()
+    {
+        return GeneralUtility::makeInstance(SortingComparisonService::class);
+    }
+
+    public function compare(array $source, array $target) {
+        $actions = [];
+
+        if (empty($source) && empty($target)) {
+            return $actions;
+        }
+        if (empty($source) && !empty($target)) {
+            foreach ($target as $item) {
+                $actions[] = [
+                    'action' => static::ACTION_ADD,
+                    'item' => $item,
+                ];
+            }
+            return $actions;
+        }
+        if (!empty($source) && empty($target)) {
+            foreach ($source as $item) {
+                $actions[] = [
+                    'action' => static::ACTION_REMOVE,
+                    'item' => $item,
+                ];
+            }
+            return $actions;
+        }
+
+        $removedItems = $this->arrayDiff($source, $target);
+        $addedItems = $this->arrayDiff($target, $source);
+        $sameItems = $this->arrayIntersect($source, $target);
+
+        foreach ($removedItems as $item) {
+            $actions[] = [
+                'action' => static::ACTION_REMOVE,
+                'item' => $item,
+            ];
+        }
+        foreach ($addedItems as $item) {
+            $actions[] = [
+                'action' => static::ACTION_ADD,
+                'item' => $item,
+            ];
+        }
+
+        if (empty($sameItems) && !empty($addedItems)) {
+            return $actions;
+        }
+
+        if ($this->isSequential($target, $sameItems)) {
+            return $actions;
+        }
+
+        $actions[] = [
+            'action' => static::ACTION_ORDER,
+            'items' => $target,
+        ];
+
+        return $actions;
+    }
+
+    protected function isSequential(array $haystack, array $needle): bool
+    {
+        foreach ($needle as $index => $item) {
+            if (!isset($haystack[$index]) || $item !== $haystack[$index]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Executes array_diff() and re-indexes array.
+     *
+     * @param array $source
+     * @param array $target
+     * @return array
+     */
+    protected function arrayDiff(array $source, array $target): array
+    {
+        $array = array_diff($source, $target);
+        return array_merge($array);
+    }
+
+    /**
+     * Executes array_intersect() and re-indexes array.
+     *
+     * @param array $source
+     * @param array $target
+     * @return array
+     */
+    protected function arrayIntersect(array $source, array $target): array
+    {
+        $array = array_intersect($source, $target);
+        return array_merge($array);
+    }
+}
---
+++ Tests/Unit/Core/Service/SortingComparisionServiceTest.php
@@ -0,0 +1,158 @@
+<?php
+namespace TYPO3\CMS\DataHandling\Tests\Unit\Core\Service;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+use TYPO3\CMS\DataHandling\Core\MetaModel\ActiveRelation;
+use TYPO3\CMS\DataHandling\Core\MetaModel\Map;
+use TYPO3\CMS\DataHandling\Core\MetaModel\PassiveRelation;
+use TYPO3\CMS\DataHandling\Core\MetaModel\Relational;
+use TYPO3\CMS\DataHandling\Core\Service\SortingComparisonService;
+use TYPO3\CMS\Extbase\Reflection\ObjectAccess;
+
+class SortingComparisonServiceTest extends UnitTestCase
+{
+    /**
+     * @var SortingComparisonService
+     */
+    protected $subject;
+
+    protected function setup()
+    {
+        parent::setUp();
+        $this->subject = SortingComparisonService::instance();
+    }
+
+    protected function tearDown()
+    {
+        unset($this->subject);
+        parent::tearDown();
+    }
+
+    /**
+     * @param array $source
+     * @param array $target
+     * @param array $expectedActions
+     * @test
+     * @dataProvider sortingActionsAreResolvedDataProvider
+     */
+    public function sortingActionsAreResolved(array $source, array $target, array $expectedActions)
+    {
+        $actions = $this->subject->compare($source, $target);
+        $this->assertEquals($expectedActions, $actions);
+    }
+
+    public function sortingActionsAreResolvedDataProvider(): array
+    {
+        return [
+            'empty source, empty target' => [
+                [],
+                [],
+                []
+            ],
+            'same source & same target' => [
+                ['A', 'B'],
+                ['A', 'B'],
+                []
+            ],
+            'filled source, empty target' => [
+                ['A', 'B'],
+                [],
+                [
+                    ['action' => SortingComparisonService::ACTION_REMOVE, 'item' => 'A'],
+                    ['action' => SortingComparisonService::ACTION_REMOVE, 'item' => 'B'],
+                ]
+            ],
+            'empty source, filled target' => [
+                [],
+                ['A', 'B'],
+                [
+                    ['action' => SortingComparisonService::ACTION_ADD, 'item' => 'A'],
+                    ['action' => SortingComparisonService::ACTION_ADD, 'item' => 'B'],
+                ]
+            ],
+            'same items re-ordering' => [
+                ['A', 'B', 'C'],
+                ['C', 'A', 'B'],
+                [
+                    ['action' => SortingComparisonService::ACTION_ORDER, 'items' => ['C', 'A', 'B']],
+                ]
+            ],
+            'removing source, add target' => [
+                ['A', 'B'],
+                ['C', 'D'],
+                [
+                    ['action' => SortingComparisonService::ACTION_REMOVE, 'item' => 'A'],
+                    ['action' => SortingComparisonService::ACTION_REMOVE, 'item' => 'B'],
+                    ['action' => SortingComparisonService::ACTION_ADD, 'item' => 'C'],
+                    ['action' => SortingComparisonService::ACTION_ADD, 'item' => 'D'],
+                ]
+            ],
+            'adding item to bottom' => [
+                ['A', 'B', 'C', 'D'],
+                ['A', 'B', 'C', 'D', 'E'],
+                [
+                    ['action' => SortingComparisonService::ACTION_ADD, 'item' => 'E'],
+                ]
+            ],
+            'adding item to top' => [
+                ['A', 'B', 'C', 'D'],
+                ['E', 'A', 'B', 'C', 'D'],
+                [
+                    ['action' => SortingComparisonService::ACTION_ADD, 'item' => 'E'],
+                    ['action' => SortingComparisonService::ACTION_ORDER, 'items' => ['E', 'A', 'B', 'C', 'D']],
+                ]
+            ],
+            'adding item between' => [
+                ['A', 'B', 'C', 'D'],
+                ['A', 'B', 'E', 'C', 'D'],
+                [
+                    ['action' => SortingComparisonService::ACTION_ADD, 'item' => 'E'],
+                    ['action' => SortingComparisonService::ACTION_ORDER, 'items' => ['A', 'B', 'E', 'C', 'D']],
+                ]
+            ],
+            'removing item from bottom' => [
+                ['A', 'B', 'C', 'D'],
+                ['A', 'B', 'C'],
+                [
+                    ['action' => SortingComparisonService::ACTION_REMOVE, 'item' => 'D'],
+                ]
+            ],
+            'removing item from top' => [
+                ['A', 'B', 'C', 'D'],
+                ['B', 'C', 'D'],
+                [
+                    ['action' => SortingComparisonService::ACTION_REMOVE, 'item' => 'A'],
+                ]
+            ],
+            'removing item between' => [
+                ['A', 'B', 'C', 'D'],
+                ['A', 'B', 'D'],
+                [
+                    ['action' => SortingComparisonService::ACTION_REMOVE, 'item' => 'C'],
+                ]
+            ],
+            'removing, adding, re-sorting' => [
+                ['A', 'B', 'C', 'D'],
+                ['A', 'E', 'D', 'C'],
+                [
+                    ['action' => SortingComparisonService::ACTION_REMOVE, 'item' => 'B'],
+                    ['action' => SortingComparisonService::ACTION_ADD, 'item' => 'E'],
+                    ['action' => SortingComparisonService::ACTION_ORDER, 'items' => ['A', 'E', 'D', 'C']],
+                ]
+            ],
+        ];
+    }
+}

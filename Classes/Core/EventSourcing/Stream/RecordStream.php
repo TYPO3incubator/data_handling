4e2f9b35068e1e9278d1637da08116ec9dc6d94b
Oliver Hader <oliver@typo3.org>
2016-08-11 15:51:31 +0200

    [TASK] Auto-detect stream-name in RecordStream

--- Classes/Core/EventSourcing/Stream/RecordStream.php
+++ Classes/Core/EventSourcing/Stream/RecordStream.php
@@ -18,6 +18,7 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\DataHandling\Core\Domain\Event\AbstractEvent;
 use TYPO3\CMS\DataHandling\Core\Domain\Event\Record;
 use TYPO3\CMS\DataHandling\Core\Domain\Event\Storable;
+use TYPO3\CMS\DataHandling\Core\Domain\Object\Identifiable;
 use TYPO3\CMS\DataHandling\Core\EventSourcing\Store\EventStore;
 use TYPO3\CMS\DataHandling\Core\Object\Instantiable;
 
@@ -52,7 +53,10 @@ class RecordStream extends AbstractStream implements Instantiable
     public function publish(AbstractEvent $event)
     {
         if ($event instanceof Storable) {
-            EventStore::instance()->append($this->name, $event);
+            EventStore::instance()->append(
+                $this->determineStreamName($event),
+                $event
+            );
         }
         foreach ($this->consumers as $consumer) {
             call_user_func($consumer, $event);
@@ -73,5 +77,26 @@ class RecordStream extends AbstractStream implements Instantiable
             $this->consumers[] = $consumer;
         }
         return $this;
+    }
+
+    /**
+     * @param Record\AbstractEvent $event
+     * @return string
+     */
+    protected function determineStreamName(Record\AbstractEvent $event): string
+    {
+        $streamName = $this->name;
+
+        // event has assigned subject
+        // (bind to whole subject identity the event is emmited for)
+        if ($event->getSubject() !== null) {
+            $streamName .= '-' . $event->getSubject()->__toString();
+        // event is identifiable, but does not have a subject
+        // (most probably used for CreatedEvent, thus bind to table-name only)
+        } elseif ($event instanceof Identifiable) {
+            $streamName .= '-' . $event->getIdentity()->getName();
+        }
+
+        return $streamName;
     }
 }

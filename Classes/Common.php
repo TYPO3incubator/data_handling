7d28d99fe712f9d71ac6ffaf6bd51787289423b7
Oliver Hader <oliver@typo3.org>
2016-08-17 01:40:26 +0200

    [TASK] Move configurations to common class

--- Classes/Common.php
+++ Classes/Common.php
@@ -23,10 +23,110 @@ class Common
     const FIELD_REVISION = 't3rev';
 
     /**
+     * @var bool
+     * @internal
+     */
+    protected static $enable = true;
+
+    /**
      * @return Dispatcher
      */
     public static function getSignalSlotDispatcher()
     {
         return GeneralUtility::makeInstance(Dispatcher::class);
+    }
+
+    /**
+     * Overrides global configuration.
+     */
+    public static function overrideConfiguration()
+    {
+        if (!static::$enable) {
+            return;
+        }
+
+        $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Origin'] =
+            $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default'];
+        $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['wrapperClass'] =
+            \TYPO3\CMS\DataHandling\Core\Compatibility\Database\ConnectionInterceptor::class;
+    }
+
+    /**
+     * Defines XCLASSES & alternative implementations.
+     *
+     * @internal
+     */
+    public static function registerAlternativeImplementations()
+    {
+        if (!static::$enable) {
+            return;
+        }
+
+        // intercepts TYPO3_DB
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Database\DatabaseConnection::class]['className']
+            = \TYPO3\CMS\DataHandling\Core\Compatibility\Database\DatabaseConnectionInterceptor::class;
+        // provides ProjectionContext, once workspace information is available
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Authentication\BackendUserAuthentication::class]['className']
+            = \TYPO3\CMS\DataHandling\Core\Authentication\BackendUserAuthentication::class;
+    }
+
+    public static function registerUpdates()
+    {
+        if (!static::$enable) {
+            return;
+        }
+
+        // create initial uuid and revision values
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update'][\TYPO3\CMS\DataHandling\Install\Updates\EventInitializationUpdate::class]
+            = \TYPO3\CMS\DataHandling\Install\Updates\EventInitializationUpdate::class;
+    }
+
+    public static function registerHooks()
+    {
+        if (!static::$enable) {
+            return;
+        }
+
+        // intercepts DataHandler
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['ac3c06f089776446875c4957a7f35a56'] =
+            \TYPO3\CMS\DataHandling\Integration\Hook\DataHandlerHook::class;
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass']['ac3c06f089776446875c4957a7f35a56'] =
+            \TYPO3\CMS\DataHandling\Integration\Hook\DataHandlerHook::class;
+    }
+
+    public static function registerSlots()
+    {
+        if (!static::$enable) {
+            return;
+        }
+
+        // provides new database fields
+        \TYPO3\CMS\DataHandling\Common::getSignalSlotDispatcher()->connect(
+            \TYPO3\CMS\Install\Service\SqlExpectedSchemaService::class, 'tablesDefinitionIsBeingBuilt',
+            \TYPO3\CMS\DataHandling\Core\Slot\EventSourcingSchemaModificationSlot::class, 'generate'
+        );
+    }
+
+    public static function registerEventSources()
+    {
+        if (!static::$enable) {
+            return;
+        }
+
+        // initialize default EventStore using SqlDriver
+        \TYPO3\CMS\DataHandling\Core\EventSourcing\Store\EventStorePool::provide()
+            ->registerDefault(
+                \TYPO3\CMS\DataHandling\Core\EventSourcing\Store\EventStore::create(
+                    \TYPO3\CMS\DataHandling\Core\EventSourcing\Store\Driver\SqlDriver::instance()
+                )
+            );
+
+        // bind stream, managing generic events
+        \TYPO3\CMS\DataHandling\Core\EventSourcing\EventManager::provide()->bind(
+            \TYPO3\CMS\DataHandling\Core\EventSourcing\Stream\StreamProvider::create('generic')
+                ->setStore(\TYPO3\CMS\DataHandling\Core\EventSourcing\Store\EventStorePool::provide()->getDefault())
+                ->setStream(\TYPO3\CMS\DataHandling\Core\EventSourcing\Stream\GenericStream::instance())
+                ->setEventNames([\TYPO3\CMS\DataHandling\Core\Domain\Event\Generic\AbstractEvent::class])
+        );
     }
 }
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,53 +1,11 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-// XCLASSES & alternative implementations
+\TYPO3\CMS\DataHandling\Common::overrideConfiguration();
+\TYPO3\CMS\DataHandling\Common::registerAlternativeImplementations();
 
-$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Origin'] =
-    $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default'];
-$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['wrapperClass'] =
-    \TYPO3\CMS\DataHandling\Core\Compatibility\Database\ConnectionInterceptor::class;
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Database\DatabaseConnection::class]['className']
-    = \TYPO3\CMS\DataHandling\Core\Compatibility\Database\DatabaseConnectionInterceptor::class;
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Authentication\BackendUserAuthentication::class]['className']
-    = \TYPO3\CMS\DataHandling\Core\Authentication\BackendUserAuthentication::class;
+\TYPO3\CMS\DataHandling\Common::registerHooks();
+\TYPO3\CMS\DataHandling\Common::registerSlots();
 
-/*
- * ext:core
- */
-
-// provide new database fields
-\TYPO3\CMS\DataHandling\Common::getSignalSlotDispatcher()->connect(
-    \TYPO3\CMS\Install\Service\SqlExpectedSchemaService::class, 'tablesDefinitionIsBeingBuilt',
-    \TYPO3\CMS\DataHandling\Core\Slot\EventSourcingSchemaModificationSlot::class, 'generate'
-);
-
-/*
- * ext:install
- */
-
-// create initial uuid values
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update'][\TYPO3\CMS\DataHandling\Install\Updates\EventInitializationUpdate::class]
-    = \TYPO3\CMS\DataHandling\Install\Updates\EventInitializationUpdate::class;
-
-// integration: hooks & slots
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['ac3c06f089776446875c4957a7f35a56'] =
-    \TYPO3\CMS\DataHandling\Integration\Hook\DataHandlerHook::class;
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass']['ac3c06f089776446875c4957a7f35a56'] =
-    \TYPO3\CMS\DataHandling\Integration\Hook\DataHandlerHook::class;
-
-// initialize default EventStore using SqlDriver
-\TYPO3\CMS\DataHandling\Core\EventSourcing\Store\EventStorePool::provide()
-    ->registerDefault(
-        \TYPO3\CMS\DataHandling\Core\EventSourcing\Store\EventStore::create(
-            \TYPO3\CMS\DataHandling\Core\EventSourcing\Store\Driver\SqlDriver::instance()
-        )
-    );
-
-// bind stream, managing generic events
-\TYPO3\CMS\DataHandling\Core\EventSourcing\EventManager::provide()->bind(
-    \TYPO3\CMS\DataHandling\Core\EventSourcing\Stream\StreamProvider::create('generic')
-        ->setStore(\TYPO3\CMS\DataHandling\Core\EventSourcing\Store\EventStorePool::provide()->getDefault())
-        ->setStream(\TYPO3\CMS\DataHandling\Core\EventSourcing\Stream\GenericStream::instance())
-        ->setEventNames([\TYPO3\CMS\DataHandling\Core\Domain\Event\Generic\AbstractEvent::class])
-);
+\TYPO3\CMS\DataHandling\Common::registerUpdates();
+\TYPO3\CMS\DataHandling\Common::registerEventSources();

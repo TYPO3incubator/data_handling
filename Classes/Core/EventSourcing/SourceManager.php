fccbe066ceb700cf1c68014a84ffd5d16abfa814
Oliver Hader <oliver@typo3.org>
2016-08-17 22:50:43 +0200

    [TASK] Integrate source manager
    For later use to mark table names as sourced by some other provider.

---
+++ Classes/Core/EventSourcing/SourceManager.php
@@ -0,0 +1,69 @@
+<?php
+namespace TYPO3\CMS\DataHandling\Core\EventSourcing;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\DataHandling\Core\Domain\Event\AbstractEvent;
+use TYPO3\CMS\DataHandling\Core\Object\Instantiable;
+use TYPO3\CMS\DataHandling\Core\Object\Providable;
+
+class SourceManager implements Providable, Instantiable
+{
+    /**
+     * @var SourceManager
+     */
+    static protected $sourceManager;
+
+    /**
+     * @param bool $force
+     * @return SourceManager
+     */
+    public static function provide(bool $force = false)
+    {
+        if (!isset(static::$sourceManager)) {
+            static::$sourceManager = static::instance();
+        }
+        return static::$sourceManager;
+    }
+    public function handle() {
+
+    }
+
+    /**
+     * @return SourceManager
+     */
+    public static function instance()
+    {
+        return GeneralUtility::makeInstance(SourceManager::class);
+    }
+
+    /**
+     * @var string[]
+     */
+    protected $sourcedTableNames = [];
+
+    /**
+     * @param string $sourcedTableName
+     * @return SourceManager
+     */
+    public function addSourcedTableName(string $sourcedTableName) {
+        if (!in_array($sourcedTableName, $this->sourcedTableNames, true)) {
+            $this->sourcedTableNames[] = $sourcedTableName;
+        }
+        return $this;
+    }
+
+    // @todo Map DataHandler CRUD command to domain events
+}

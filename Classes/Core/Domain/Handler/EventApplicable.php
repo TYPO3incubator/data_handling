ff212e7fd677a2bb3702ad2ce68bca3716e9665c
Oliver Hader <oliver@typo3.org>
2016-08-23 01:17:45 +0200

    [TASK] Add domain handler interfaces

---
+++ Classes/Core/Domain/Handler/EventApplicable.php
@@ -0,0 +1,31 @@
+<?php
+namespace TYPO3\CMS\DataHandling\Core\Domain\Handler;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\DataHandling\Core\Domain\Event\AbstractEvent;
+
+interface EventApplicable
+{
+    /**
+     * @param $subject
+     * @return $this
+     */
+    public function setSubject($subject);
+
+    /**
+     * @param AbstractEvent $event
+     */
+    public function apply(AbstractEvent $event);
+}

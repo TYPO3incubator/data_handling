67c15592ad1a3b7c1fffbb445a927b14efcf3ace
Oliver Hader <oliver@typo3.org>
2016-08-27 10:34:25 +0200

    [WIP][TASK] Integrate meta projections

---
+++ Classes/Core/Process/Projection/GenericEntityProjectionProvider.php
@@ -0,0 +1,63 @@
+<?php
+namespace TYPO3\CMS\DataHandling\Core\Process\Projection;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\DataHandling\Core\Framework\Process\Projection\ProjectionProvidable;
+
+class GenericEntityProjectionProvider implements ProjectionProvidable
+{
+    /**
+     * @var array
+     */
+    protected $options;
+
+    /**
+     * @var \Closure[]|callable
+     */
+    protected $streamListeners = [];
+
+    /**
+     * @var \Closure[]|callable
+     */
+    protected $eventListeners = [];
+
+    /**
+     * @param array $options
+     * @param array $streamListeners
+     * @param array $eventListeners
+     */
+    public function __construct(array $options, array $streamListeners, array $eventListeners)
+    {
+        $this->options = $options;
+        $this->streamListeners = $streamListeners;
+        $this->eventListeners = $eventListeners;
+    }
+
+    /**
+     * @return \TYPO3\CMS\DataHandling\Core\Framework\Process\Projection\StreamProjecting
+     */
+    public function forStream()
+    {
+        $projection = GenericEntityStreamProjection::instance();
+    }
+
+    /**
+     * @return \TYPO3\CMS\DataHandling\Core\Framework\Process\Projection\EventProjecting
+     */
+    public function forEvent()
+    {
+        $projection = GenericEntityEventProjection::instance();
+    }
+}
---
+++ Classes/Core/Process/Projection/GenericEntityStreamProjection.php
@@ -0,0 +1,37 @@
+<?php
+namespace TYPO3\CMS\DataHandling\Core\Process\Projection;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\DataHandling\Core\EventSourcing\Stream\EventStream;
+use TYPO3\CMS\DataHandling\Core\Framework\Process\Projection\StreamProjecting;
+
+class GenericEntityStreamProjection extends AbstractGenericEntityProjection  implements StreamProjecting
+{
+    /**
+     * @return GenericEntityEventProjection
+     */
+    static public function instance()
+    {
+        return GeneralUtility::makeInstance(GenericEntityEventProjection::class);
+    }
+
+    /**
+     * @param EventStream $stream
+     */
+    public function project(EventStream $stream) {
+        // TODO: Implement project() method.
+    }
+}

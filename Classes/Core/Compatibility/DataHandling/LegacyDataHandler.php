5dcf63a0039ed820e9adccfa3cbfbc95bd90f0ad
Oliver Hader <oliver@typo3.org>
2016-09-08 23:27:26 +0200

    [BUGFIX] DateTime values are not "initialized" for data-handling

--- Classes/Core/Compatibility/DataHandling/LegacyDataHandler.php
+++ Classes/Core/Compatibility/DataHandling/LegacyDataHandler.php
@@ -14,12 +14,40 @@ namespace TYPO3\CMS\DataHandling\Core\Compatibility\DataHandling;
  * The TYPO3 project - inspiring people to share!
  */
 
+use TYPO3\CMS\Core\Database\Query\QueryHelper;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 
 class LegacyDataHandler extends DataHandler
 {
+    /**
+     * @param string $value
+     * @param array $tcaFieldConf
+     * @param string $table
+     * @param int $id
+     * @param int $realPid
+     * @param string $field
+     * @return array
+     */
     public function checkValueForInput($value, $tcaFieldConf, $table, $id, $realPid, $field)
     {
+        $value = $this->modifyDateTimeValue($value, $tcaFieldConf);
         return parent::checkValueForInput($value, $tcaFieldConf, $table, $id, $realPid, $field);
+    }
+
+    /**
+     * @param string $value
+     * @param $configuration
+     * @return string
+     */
+    private function modifyDateTimeValue(string $value, array $configuration)
+    {
+        $dateTimeFormats = QueryHelper::getDateTimeFormats();
+        $databaseType = ($configuration['dbType'] ?? null);
+        if ($databaseType === 'date' || $databaseType === 'datetime') {
+            $emptyValue = $dateTimeFormats[$databaseType]['empty'];
+            $format = $dateTimeFormats[$databaseType]['format'];
+            $value = (!empty($value) && $value !== $emptyValue ? gmdate($format, $value) : $emptyValue);
+        }
+        return $value;
     }
 }

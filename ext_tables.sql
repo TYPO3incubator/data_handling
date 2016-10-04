5f74de77811cac8f9ea7d26b4ac74bc55bcbcabf
Oliver Hader <oliver@typo3.org>
2016-10-04 12:46:56 +0200

    [TASK] Remove uid column from event store

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,7 +2,6 @@
 # Table structure for table 'sys_event_store'
 #
 CREATE TABLE sys_event_store (
-	uid int(11) unsigned NOT NULL auto_increment,
 	event_stream varchar(128) NOT NULL,
 	event_categories varchar(256) DEFAULT NULL,
 	event_id varchar(36) NOT NULL,
@@ -11,9 +10,7 @@ CREATE TABLE sys_event_store (
 	event_date TIMESTAMP(6) NOT NULL,
 	aggregate_id varchar(36) DEFAULT NULL,
 	data mediumtext,
-	metadata text,
-
-	PRIMARY KEY (uid)
+	metadata text
 );
 
 #
